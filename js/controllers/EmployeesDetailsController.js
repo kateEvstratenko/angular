62e57bfddd7eda955fc6751f0fd743450bd24dc6
kate <kate_evstratenko@mail.ru>
2014-08-09 20:22:55 +0300

    filter for first name

---
+++ js/controllers/EmployeesDetailsController.js
@@ -0,0 +1,3 @@
+﻿angular.module('employeeApp').controller('EmployeesDetailsController', function ($scope, $routeParams) {
+    $scope.employee_id = $routeParams.id;
+});
---
+++ js/services/Initialize.js
@@ -0,0 +1,18 @@
+﻿angular.module('employeeApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+
+        $routeProvider.when('/employees/:id', {
+            controller: 'EmployeesDetailsController',
+            templateUrl: 'details.html',
+        });
+
+        $routeProvider.when('/', {
+            controller: 'EmployeesListController',
+            templateUrl: 'employeesList.html'
+        });
+        
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    }
+);
